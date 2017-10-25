9fb0280be2a06af5f501ffc826b67ec45c517197
HOA <hoa@lifestyle-design.jp>
2017-10-25 18:21:34 +0900

    step 1
    success callback oauth

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/oauth-setting', 'UserController@getUsers');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/auth/facebook', 'Auth\LoginController@redirectToProvider');
+Route::get('/auth/facebook/callback', 'Auth\LoginController@handleProviderCallback');
